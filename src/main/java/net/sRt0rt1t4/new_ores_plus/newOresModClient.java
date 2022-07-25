e1c3159e42a9a818d843b8b818ce6454827fb66d
crist <c.alcaide.coret@gmail.com>
2022-07-26 00:45:02 +0200

    -Created a Client class

---
+++ src/main/java/net/sRt0rt1t4/new_ores_plus/newOresModClient.java
@@ -0,0 +1,10 @@
+package net.sRt0rt1t4.new_ores_plus;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class newOresModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
